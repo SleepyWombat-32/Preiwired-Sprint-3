8b02200bb60c1e2e35564c532dd540da87886e40
SleepyWombat-32 <85896260+SleepyWombat-32@users.noreply.github.com>
2021-07-04 17:02:32 +0100

    No music
    Removed all music for the time being

--- 3D RPG/Background.gd
+++ 3D RPG/Background.gd
@@ -2,4 +2,6 @@ extends AudioStreamPlayer
 # Background music, handles the music played in the main part of the game 
 
 func _ready(): 
-	$AnimationPlayer.play("FadeIn")
+	#$AnimationPlayer.play("FadeIn")
+	print ("music muted")
+	pass
